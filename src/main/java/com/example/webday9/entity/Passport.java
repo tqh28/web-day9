d0c7f222d17be648de10c21db44805437ffd1b0d
Huy Tran <huy.tran@evizi.com>
2020-10-18 09:29:20 +0700

    un-comment code

--- src/main/java/com/example/webday9/entity/Passport.java
+++ src/main/java/com/example/webday9/entity/Passport.java
@@ -34,12 +34,12 @@ public class Passport {
         this.number = number;
     }
 
-//    public Student getStudent() {
-//        return student;
-//    }
-//
-//    public void setStudent(Student student) {
-//        this.student = student;
-//    }
+	public Student getStudent() {
+		return student;
+	}
+
+	public void setStudent(Student student) {
+		this.student = student;
+	}
 
 }
